792ab11c2afb34af19a5527c93c2920697ef4104
ebayboy <552999964@.com>
2019-04-23 03:28:17 +0000

    fw

--- vendor/build-hyperscan-fat.sh
+++ vendor/build-hyperscan-fat.sh
@@ -1,25 +1,24 @@
 
-CURDIR=$(pwd)
-
-BUILDDIR=$CURDIR/vendor/hyperscan/hs_build
+BUILDDIR=hyperscan/hs_build
 
 #FileType=None
 #FileType=Debug
 #FileType=MinSizeRel
-#FileType=RelWithDebInfo
 FileType=Release
+#FileType=RelWithDebInfo
 
-tar xzvf boost.tar.gz
-tar xzvf hyperscan.tar.gz
+CURDIR=$(pwd)
+
+#ln -s $(pwd)/boost_1_64_0/boost hyperscan/include/boost
 
 rm -fr $BUILDDIR && mkdir $BUILDDIR
 cd $BUILDDIR && cmake ../                           \
+                    -DBUILD_SHARED_LIBS=on          \
                     -DCMAKE_BUILD_TYPE=$FileType    \
                     -DCMAKE_C_FLAGS="-fPIC "        \
                     -DCMAKE_CXX_FLAGS="-fPIC "      \
                     -DFAT_RUNTIME=1                 \
                 && make -j 4                        \
-                && cp lib/libhs.a ${CURDIR}         \
+                && make install                     \
                 && cd - || exit 1
-
 
